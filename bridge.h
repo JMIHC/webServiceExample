64e73fe6f33d42db7a27acd21276d508aa44e7e3
jmihc <john@bahai.us>
2016-09-07 17:36:33 -0500

    Integrating swiftyJSON and bridging with AFNetworking

---
+++ bridge.h
@@ -0,0 +1,13 @@
+//
+//  bridge.h
+//  webServiceExample
+//
+//  Created by John Cornyn on 9/7/16.
+//  Copyright © 2016 Johnny Cornyn. All rights reserved.
+//
+
+#ifndef bridge_h
+#define bridge_h
+#import "AFNetworking/AFNetworking.h"
+
+#endif /* bridge_h */
--- webServiceExample/ViewController.swift
+++ webServiceExample/ViewController.swift
@@ -9,12 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var forecastLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let manager = AFHTTPSessionManager()
+        
+        manager.GET("http://api.openweathermap.org/data/2.5/forecast/city?id=524901&APPID=7ae26182752050a0c0c32b9c8e0de957",
+                    parameters: nil,
+                    progress: nil,
+                    success: { (operation: NSURLSessionDataTask,responseObject: AnyObject?) in
+                        if let responseObject = responseObject {
+                            
+                            let json = JSON(responseObject)
+                            if let forecast = json["list"][0]["weather"][0]["description"].string {
+                                self.forecastLabel.text = forecast
+                            }
+                        }
+        },
+                    failure: { (operation: NSURLSessionDataTask?,error: NSError) in
+                        print("Error: " + error.localizedDescription)
+        })
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
